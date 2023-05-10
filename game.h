e407265d1e2c1f9c3d84c64454480d28df2b8c36
KalebTangen <ktangen@cub.uca.edu>
2023-05-10 15:33:06 -0500

    Added aliens
    added alien target, and the game object

---
+++ game.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// Includes
+# include <iostream>
+# include <cmath>
+# include <string>
+# include <iomanip>
+# include <fstream>
+# include <Player.h>
+# include <Target.h>
+
+using namespace std;
